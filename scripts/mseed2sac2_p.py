c99881142468dc246f5e3b35f6650b2bb2ba9c25
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2022-05-09 17:04:02 +0800

    Update mseed2sac2_p.py

--- scripts/mseed2sac2_p.py
+++ scripts/mseed2sac2_p.py
@@ -348,6 +348,8 @@ def convert_hourly(hour_files_path):
 
 			ibeg = 0
 			starttime = starttime_org
+			endtime = UTCDateTime(starttime.year, starttime.month, starttime.day, 23, 59, 59, 999999)
+
 
 			iday1 = starttime_org.julday
 			iday2 = endtime_org.julday
@@ -356,8 +358,8 @@ def convert_hourly(hour_files_path):
 				#endtime = UTCDateTime(starttime.year, starttime.month, starttime.day, 23, 59, 59, 999999)
 				#midtime = starttime + 0.5*seconds_daily
 				#endtime = UTCDateTime(midtime.year, midtime.month, midtime.day, 23, 59, 59, 999999)
-				endtime = UTCDateTime(year=starttime.year, julday=starttime.julday, hour=23, \
-				                                     minute=59, second=59, microsecond=999999)
+				#endtime = UTCDateTime(year=starttime.year, julday=starttime.julday, hour=23, \
+				#                                     minute=59, second=59, microsecond=999999)
 				elapsed_time = endtime - starttime
 
 				iend = min(ibeg + int(elapsed_time*df), npts_org)
@@ -413,6 +415,7 @@ def convert_hourly(hour_files_path):
 
 				npts = len(tr_out.data)
 				starttime = starttime + npts*dt
+				endtime = starttime + seconds_daily
 				ibeg = iend + 1
 
 				del tr_out
