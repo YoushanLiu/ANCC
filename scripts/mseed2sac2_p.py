282d29dcb4131dda272d5ada49014d8198473bfc
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2022-05-10 00:10:35 +0800

    Update mseed2sac2_p.py

--- scripts/mseed2sac2_p.py
+++ scripts/mseed2sac2_p.py
@@ -415,7 +415,7 @@ def convert_hourly(hour_files_path):
 
 				npts = len(tr_out.data)
 				starttime = starttime + npts*dt
-				endtime = starttime + seconds_daily
+				endtime = starttime + seconds_daily - dt
 				ibeg = iend + 1
 
 				del tr_out
