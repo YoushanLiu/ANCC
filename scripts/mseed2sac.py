5f60232b2898b65077031d8e53c523c86c61f11e
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2022-06-03 01:07:12 +0800

    Update mseed2sac.py

--- scripts/mseed2sac.py
+++ scripts/mseed2sac.py
@@ -405,7 +405,8 @@ def convert_hourly(hour_files_path):
 					# write network name
 					sac.knetwk = network_name
 
-					sac.reftime += sac.b
+					#sac.reftime += sac.b
+					sac.reftime = tr_out.stats.starttime
 
 
 					# write sac
