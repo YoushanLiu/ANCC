6664dbff63e32fc92b1a26cb2950b09865e5d802
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2022-07-10 19:47:51 +0800

    Add files via upload

---
+++ ANCC/compile.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+rm -rf *.lst
+
+cd ./src
+#############################################################################
+
+####################################
+echo
+echo "Enter into AFTAN ... "
+cd AFTAN
+make cleanall
+make
+make install
+make clean
+cd ../
+####################################
+
+####################################
+echo
+echo "Enter into AND ... "
+cd AND
+make cleanall
+make
+make install
+make clean
+cd ../
+####################################
+
+####################################
+echo
+echo "Enter into TF_PWS ... "
+cd TF_PWS
+make cleanall
+make
+make install
+make clean
+cd ../
+####################################
+
+#############################################################################
+
+####################################
+echo
+echo "Compiling done ... "
