c096ec8ed2870c30fb73a89b97b50c4ad17427fd
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2023-04-20 16:14:16 +0800

    Update cutdata2.py

--- scripts/cutdata2.py
+++ scripts/cutdata2.py
@@ -280,7 +280,7 @@ def cutdata_daily(station_path):
 
 				# create the path if it does not exist
 				if (not os.path.exists(mypath)):
-					os.makedirs(mypath)
+					os.makedirs(mypath, exist_ok=True)
 
 
 				outfile = mypath + sac_filename
