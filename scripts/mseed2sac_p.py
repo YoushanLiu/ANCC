4b2f91f8df04b486c1c6e2046c7de4c69f16d506
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2023-11-07 11:35:57 +0800

    Update mseed2sac_p.py

--- scripts/mseed2sac_p.py
+++ scripts/mseed2sac_p.py
@@ -211,6 +211,7 @@ def convert_hourly(hour_files_path):
 	hour_files_list = os.listdir(hour_files_path)
 	#hour_files_list = glob.glob(hour_files_path + 'E*.*')
 
+	idx = findstr(hour_files_path, '/')
 
 	for hour_file in hour_files_list:
 
@@ -225,7 +226,7 @@ def convert_hourly(hour_files_path):
 		starts = [each.start() for each in re.finditer(sac_suffix, hour_file.upper())]
 		ends = [start+len(sac_suffix) - 1 for start in starts]
 		span = [(start, end) for start,end in zip(starts, ends)]
-		is_sac = is_sacfile and (len(span) >= 1)
+		is_sacfile = is_sacfile and (len(span) >= 1)
 
 		if (is_sacfile):
 			print('is not a sacfile')
@@ -267,7 +268,7 @@ def convert_hourly(hour_files_path):
 			try:
 				tr = st[i]
 				# remove round error
-				tr.stats.delta = round(tr.stats.delta*1e6)*1.e-6
+				tr.stats.delta = round(tr.stats.delta*1e3)*1.e-3
 			except:
 				continue
 
@@ -297,7 +298,7 @@ def convert_hourly(hour_files_path):
 					print("Error: decimate factor can only be integer !")
 				if (decimate_factor > 1):
 					# Nyquist frequency of the downsampling rate
-					freq_lowpass = 0.499 * downsampling_rate
+					freq_lowpass = 0.49 * downsampling_rate
 					if (not(is_bandpass and (fhigh <= freq_lowpass))):
 						tr.filter('lowpass', freq=freq_lowpass, corners=2, zerophase=True)
 					tr.decimate(factor=decimate_factor, strict_length=False, no_filter=True)
@@ -318,7 +319,7 @@ def convert_hourly(hour_files_path):
 			#	# try to extract station name based on folder name
 			#	ipos = -1
 			#	for j in range(len(sta.name)):
-			#		res = findstr(hour_files_path[nrootdir:-1], sta.name[j])
+			#		res = findstr(hour_files_path[nrootdir:idx[-2]], sta.name[j])
 			#		if ([] != res):
 			#			ipos = j
 			#			break
@@ -329,7 +330,7 @@ def convert_hourly(hour_files_path):
 
 			ipos = -1
 			for j in range(len(sta.name)):
-				res = findstr(hour_files_path[nrootdir:-1], sta.name[j])
+				res = findstr(hour_files_path[nrootdir:idx[-2]], sta.name[j])
 				if ([] != res):
 					ipos = j
 					break
