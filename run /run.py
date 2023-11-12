a183e7c2fd8c71288691f07282f3128952f4d1cd
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2023-11-12 20:28:33 +0800

    Update run.py

--- run /run.py
+++ run /run.py
@@ -3,15 +3,15 @@
 #=======================================================================================
 #=======================================================================================
 # This script is the only one that you need to execute to process the seismic ambient
-# noise data with parallel computing. It will invoke the <AND_Driver>, <TF_PWS> and
-# <AFTAN_PROG> three executables to deal with the input seismic waveforms based on input
+# noise data with parallel computing. It will invoke the <ANCC>, <TF_PWS> and
+# <AFTAN> three executables to deal with the input seismic waveforms based on input
 # parameters as defined in <input.dat>. Detailed description of each individual parameter
 # can be found in <input.dat>. The output of this program is cross-correlation functions
 # and corresponding group and phase velocity dispersion data.
 #=======================================================================================
 # The path of input data folder is given in the <SACfolder> variable in the following
 # script. The data structure should be */year/month/event/net.sta.cha.SAC
-# e.g. DATA/2018/01/20180101_120000/DB.EW01.LHZ.SAC
+# e.g. DATA/2018/01/20180101_120000/DB.EW01.BHZ.SAC
 #=======================================================================================
 # Individual or combined full RESP or PZ file(s) should be put in <PZfolder>.
 #=======================================================================================
@@ -24,7 +24,7 @@
 # dispersion data. The output 'BOOTSTRAP' folder contains all the bootstrap data (optional).
 #=======================================================================================
 # Required auxiliary files and programs:
-#   sac
+#   SAC
 #   input.dat
 #   ref1Dmod.dat
 #=======================================================================================
@@ -33,7 +33,7 @@
 #        analysis part. The phase-weighted stacking program is modified from Guoliang Li'
 #        code (guoleonlee@gmail.com)
 #        Any bugs, suggestions or comments related to this program can be directed
-#        to Xingli Fan via <fanxldengcm@gmail.com>.
+#        to Xingli Fan via <fanxldengcm@gmail.com> and <ysliu@mail.iggcas.ac.cn>.
 #=======================================================================================
 #=======================================================================================
 
@@ -64,25 +64,22 @@ nprocs = multiprocessing.cpu_count()
 
 ###########################################################
 
-#BIN_DIR = os.environ['HOME'] + '/bin'
-## create links
-#if (not os.path.exists(os.getcwd() + '/AFTAN')):
-#	os.symlink(BIN_DIR + '/AFTAN', 'AFTAN')
-#else:
-#	os.remove('AFTAN')
-#	os.symlink(BIN_DIR + '/AFTAN', 'AFTAN')
-#
-#if (not os.path.exists(os.getcwd() + '/ANCC')):
-#	os.symlink(BIN_DIR + '/ANCC', 'ANCC')
-#else:
-#	os.remove('ANCC')
-#	os.symlink(BIN_DIR + '/ANCC', 'ANCC')
-#
-#if (not os.path.exists(os.getcwd() + '/TF_PWS')):
-#	os.symlink(BIN_DIR + '/TF_PWS', 'TF_PWS')
-#else:
-#	os.remove('TF_PWS')
-#	os.symlink(BIN_DIR + '/TF_PWS', 'TF_PWS')
+def check_autocorrelation(filename):
+	try:
+		line = linecache.getline(filename, 46)
+	except:
+		return False
+	regexp = re.compile("r[^#]+[ \t\w{,3}]+")
+	ans = regexp.search(line[0:10])
+	if (ans is None):
+		return False
+	if (ans.strip().upper() in "YES"):
+		is_auto_correlation = True
+	else:
+		is_auto_correlation = False
+	return is_auto_correlation
+
+is_auto_correlation = check_autocorrelation(filename)
 
 ###########################################################
 
@@ -94,55 +91,6 @@ os.system('find %s -name "*" -type f -size 0c | xargs -n 1 rm -f' % (SACfolder))
 # Parse the station and event informations.
 ###########################################################
 os.system('rm -rf stations.tmp events.lst')
-
-
-
-#def scan_daily(day):
-#    day_folder = month_folder + '/' + day
-#    sacfiles = day_folder + '/*.SAC'
-#    file_list = glob.glob(sacfiles)
-#    if (len(file_list) <= 1):
-#        print("skip %s because of single station folder\n"%(day_folder))
-#        return
-#    os.system("saclst knetwk kstnm stlo stla delta f %s | awk '{print $2,$3,$4,$5,$6}' >> stations.tmp"%(sacfiles))
-#    os.system("ls %s -d >> events.lst"%(day_folder))
-
-
-#print("\n")
-#for year in os.listdir(SACfolder):
-#    year_folder = SACfolder + '/' + year
-#    for month in os.listdir(year_folder):
-#        month_folder = year_folder + '/' + month
-
-#        day_folders_list = os.listdir(month_folder)
-
-#        pool = ThreadPool()
-#        pool.map(scan_daily, day_folders_list)
-#        pool.close()
-#        pool.join()
-
-#os.system("sort stations.tmp | uniq > stations.lst")
-#os.system("rm -rf stations.tmp")
-
-
-
-def check_autocorrelation(filename):
-	try:
-		line = linecache.getline(filename, 46)
-	except:
-		return False
-	regexp = re.compile("r[^#]+[ \t\w{,3}]+")
-	ans = regexp.search(line)
-	if (ans is None):
-		return False
-	if (ans.strip().upper() in "YES"):
-		is_auto_correlation = True
-	else:
-		is_auto_correlation = False
-	return is_auto_correlation
-
-is_auto_correlation = check_autocorrelation(filename)
-
 
 print("\n")
 ##awkstr = " | awk '{printf \"%8s  %8s %8.4f %8.4f %5.3f\",$2,$3,$4,$5,$6}' >> stations.tmp"
