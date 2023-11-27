1d1b08d6e9c73cb817cbae172594b14e243a33de
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2023-11-27 22:07:23 +0800

    Update mseed2sac2_p.py

--- scripts/mseed2sac2_p.py
+++ scripts/mseed2sac2_p.py
@@ -126,7 +126,7 @@ channel_name = ['BHZ', 'BHN', 'BHE']
 # read station list
 def read_station_list(filename):
 
-	class station:
+	class Station:
 		def __init__(self):
 			self.name = []
 			self.stla = []
@@ -140,7 +140,7 @@ def read_station_list(filename):
 	stlo = ''
 	stel = ''
 	netwk = ''
-	sta = station()
+	sta = Station()
 	with open(filename, 'r') as f:
 		lines = f.readlines()
 	for line in lines[1:]:
@@ -267,7 +267,7 @@ def convert_hourly(hour_files_path):
 			try:
 				tr = st[i]
 				# remove round error
-				tr.stats.delta = round(tr.stats.delta*1e3)*1.e-3
+				tr.stats.delta = round(tr.stats.delta*1e6)*1.e-6
 			except:
 				continue
 
