0fcd2498c6c52dacd35cd9a3f0d54809f53ae8dc
Youshan Liu <ysliu@mail.iggcas.ac.cn>
2023-11-29 17:08:56 +0800

    Update cutdata_p.py

--- scripts/cutdata_p.py
+++ scripts/cutdata_p.py
@@ -210,7 +210,7 @@ def merge_data(hour_files_list):
 def cutdata_daily(day_folder):
 
 	day_path = station_stage_path + day_folder + '/'
-	print('\tEntering directory ' + day_path[len_rootdir:-1])
+	print('Entering directory ' + day_path[len_rootdir:-1])
 	#print('\n')
 
 	if (not os.path.isdir(day_path)):
@@ -319,9 +319,9 @@ def cutdata_daily(day_folder):
 
 
 		del tr, hour_files_list
-		print('\t\t%s is done ... \n' % date2str(starttime_daily))
+		print('%s is done ... \n' % date2str(starttime_daily))
 
-	print('\tLeaving directory ' + day_path[len_rootdir:-1])
+	print('Leaving directory ' + day_path[len_rootdir:-1])
 	#print('\n')
 
 	return
